9148f951d00ff66932b7a296e5f9fc5e5ff0c8c7
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-08-13 16:56:17 +0100

    Sign-up system
    - Development of sign-up system started (BROKEN)

---
+++ php/signup.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Sign Up</title>
+</head>
+<body>
+    <section class="signup-form">
+    <h2>Sign Up</h2>
+    <form action="signup.inc.php" method="post">
+        <input type="text" name="name" placeholder="Username">
+        <input type="text" name="email" placeholder="Email">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="password" name="pwdrepeat" placeholder="Confirm Password">
+        <button type="submit" name="submit">Sign Up</button>
+    </form>
+</body>
+</section>
