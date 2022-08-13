c8e8cd73dd9ade831046924a5f560e4ba8638832
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-08-13 19:59:16 +0100

    Try to move style for php to separate css file

--- php/signup.php
+++ php/signup.php
@@ -2,7 +2,7 @@
 <html>
 
 <div class="topnav">
-    <img class="logo" src="images/condensed-tunedtv.jpg">
+    <img class="logo" src="../images/condensed-tunedtv.jpg">
     <a class="active" href="#home">Home</a>
     <a href="html/leagues.html">Leagues</a>
     <a href="html/premium.html">Premium</a>
@@ -14,48 +14,7 @@
   <meta charset="UTF-8">
   <link rel="stylesheet" href="../css/style.css">
     <title>TunedTV</title>
-  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-  <style>
-    /* Site-wide font */
-*{
-  font-family: Montserrat;
-}
-
-/* Logo formatting */
-.logo {
-  vertical-align: top;
-  width: 100px;
-  height: 50px;
-  float: left;
-}
-
-/* Add a black background color to the top navigation */
-.topnav {
-    background-color: rgb(0, 0, 0);
-    overflow: hidden;
-  }
-  
-  /* Style the links inside the navigation bar */
-  .topnav a {
-    float: left;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-  }
-  
-  /* Change the color of links on hover */
-  .topnav a:hover {
-    background-color: #ddd;
-    color: black;
-  }
-  
-  /* Add a color to the active/current link */
-  .topnav a.active {
-    background-color: #ffffff;
-    color: rgb(0, 0, 0);
-</style>
+  <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 <body>
     <section class="signup-form">
