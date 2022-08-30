4667b9a3a9c381fc84e19b4ce5169e9582931ea0
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-08-30 18:55:41 +0100

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -70,7 +70,7 @@ function uidExists($conn, $name, $email) {
 
 function createUser($conn, $name, $email, $pwd) {
     var_dump($conn);
-    $sql = "INSERT INTO users (usersEmail, usersUid, usersPwd) VALUES (?, ?, ?);";
+    $sql = "INSERT INTO users (usersEmail, usersUid, usersPwd) VALUES (?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../php/signup.php?error=stmtfailed");
