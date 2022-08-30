c97e74848133599f8f5f5b56227ae67318a69162
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-08-30 13:28:11 +0100

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -69,7 +69,7 @@ function uidExists($conn, $name, $email) {
 }
 
 function createUser($conn, $name, $email, $pwd) {
-    var_dump($conn);exit;
+    var_dump($conn);
     $sql = "INSERT INTO users (usersEmail, usersUid, usersPwd) VALUES (?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
