92807e568330a4418cda1baafe4cffddcd52b867
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-08-30 20:09:07 +0100

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -73,7 +73,7 @@ function createUser($conn, $name, $email, $pwd) {
     $sql = "INSERT INTO users (usersEmail, usersUid, usersPwd) VALUES (?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../php/signup.php?error=stmtfailed");
+        //header("location: ../php/signup.php?error=stmtfailed");
         exit();
     }
 
