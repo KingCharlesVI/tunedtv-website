0c1eea53ed65dfc4b9d8b3cba4ac52459c8660e7
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-09-03 21:03:50 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,21 @@
 <?php 
     session_start();
+    include_once 'header.php';
   
     if(!$_SESSION['id']){
         header('location:login.php');
     }
- 
 ?>
- 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>TunedTV</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+  </head>
+<body>
 <h1>Welcome <?php echo ucfirst($_SESSION['first_name']); ?></h1>
 <a href="logout.php?logout=true">Logout</a>
+</body>
+</html>
