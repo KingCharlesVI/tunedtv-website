a8d3c20216f33ac8e0a59f12bfe808aa287ee6a7
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2022-09-13 20:41:07 +0100

    Hotfixes

---
+++ static/construction.php
@@ -0,0 +1,19 @@
+<?php
+  include_once '../header.php'
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+      <link rel="stylesheet" href="../css/construction.css">
+      <title>TunedTV</title>
+  </head>
+<body>
+	<div class="bgimg">
+        <div class="middle">
+          <h1>COMING SOON</h1>
+          <hr>
+          <p>This page is under construction</p>
+        </div>
+</body>
+</html>
